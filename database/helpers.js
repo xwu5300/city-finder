bbd10fca99dca1388c20e1dbb68de425961299dc
Tom Wagner <wagne868@umn.edu>
2018-04-23 14:59:41 -0400

    Move makeQueryString to helpers.js

--- database/helpers.js
+++ database/helpers.js
@@ -1,8 +1,40 @@
 const axios = require('axios');
 const YQL = require('YQL');
 
-// Returns the weatherdata for the cities. Currently, the result of this function is logged to the console on the client side
+let makeQueryString = (props) => {
+  // called every time there is a change in state (aka a filter is selected/deselected)
+  // returns a string that is used to query the database 
+
+  // example query string: {"$and": [{"$or": [{"region": "Northeast"}, {"region": "Rockies"}]},
+  //                                 {"$or": [{"temperature": "hot"}, {"temperature": "mild"}]}]}
+
+  let oneCategoryQuery = []
+  let queries = (props ? JSON.parse(props) : {}) //if no filters selected, eventually return "{}"
+  for (let category in queries) { 
+    let oneQuery = [];
+    if (queries[category].length > 0) {
+      queries[category].forEach((selection) => {
+        let oneFilter = {};
+        oneFilter[category] = selection;
+        oneQuery.push(oneFilter);
+      })
+      let oneCategory = {}
+      oneCategory["$or"] = oneQuery; //return cities that match filters in one category (e.g., hot or mild if both are selected)
+      oneCategoryQuery.push(oneCategory)
+    }
+  }
+  let allFilters = {}
+  if(oneCategoryQuery.length > 0){ 
+    allFilters["$and"] = oneCategoryQuery;
+  } 
+  return JSON.stringify(allFilters)
+}
+
+// Returns weatherData for the cities. Currently, the result of this function is logged to the console on the client side
 fetchWeather = function(cityIDs, callback) {
+  // API DOCUMENTATION LINKS FOR fetchWeather:
+  // https://gist.github.com/ydn/6ef5a695e871b8a628d0#file-weather-js
+  // https://developer.yahoo.com/weather/
 
   var query = new YQL(`SELECT * FROM weather.forecast where woeid in(${cityIDs})`)
   query.exec((err, data) => {
@@ -39,8 +71,5 @@ fetchWeather = function(cityIDs, callback) {
   });
 }
 
+exports.makeQueryString = makeQueryString;
 exports.fetchWeather = fetchWeather;
-
-// API DOCUMENTATION LINKS:
-// https://gist.github.com/ydn/6ef5a695e871b8a628d0#file-weather-js
-// https://developer.yahoo.com/weather/
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const {fetchWeather} = require(path.join(__dirname + '/../database/helpers.js'));
+const { fetchWeather, makeQueryString } = require(path.join(__dirname + '/../database/helpers.js'));
 const DB = require(path.join(__dirname + '/../database/database.js'));
 
 const app = express();
@@ -54,33 +54,3 @@ app.get('/weather', (req, res) => {
 app.listen(process.env.PORT || 3000, () => {
   console.log('server listening on 3000!')
 })
-
-
-let makeQueryString = (props) => {
-  // called everytime there is a change in state (aka a filter is selected/deselected)
-  // returns a string that is used to query the database 
-
-  // example query string: {"$and": [{"$or": [{"region": "Northeast"}, {"region": "Rockies"}]},
-  //                                 {"$or": [{"temperature": "hot"}, {"temperature": "mild"}]}]}
-
-  let oneCategoryQuery = []
-  let queries = (props ? JSON.parse(props) : {}) //if no filters selected, eventually return "{}"
-  for (let category in queries) { 
-    let oneQuery = [];
-    if (queries[category].length > 0) {
-      queries[category].forEach((selection) => {
-        let oneFilter = {};
-        oneFilter[category] = selection;
-        oneQuery.push(oneFilter);
-      })
-      let oneCategory = {}
-      oneCategory["$or"] = oneQuery; //return cities that match filters in one category (e.g., hot or mild if both are selected)
-      oneCategoryQuery.push(oneCategory)
-    }
-  }
-  let allFilters = {}
-  if(oneCategoryQuery.length > 0){ 
-    allFilters["$and"] = oneCategoryQuery;
-  } 
-  return JSON.stringify(allFilters)
-}
