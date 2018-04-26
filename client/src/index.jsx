ca35dd6bdbcafe7b1215d9940ddbdc34a444057b
brianlee <blee@nyu.edu>
2018-04-26 19:26:54 -0400

    Fully merged branch

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,9 +3,5 @@ import ReactDOM from "react-dom";
 import App from "./app.jsx";
 import WordCloudComp from './WordCloudComp.jsx';
 
-<<<<<<< HEAD
-ReactDOM.render(<App />, document.getElementById("app"));
-=======
 ReactDOM.render(<App />, document.getElementById("app"));
 // ReactDOM.render(<WordCloudComp/>, document.getElementById("test"));
->>>>>>> tweetCloud
