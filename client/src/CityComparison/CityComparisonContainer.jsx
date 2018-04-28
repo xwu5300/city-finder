baa04de14c8f9ce2e15a8a1e6acdbca46d80d05e
Xiang Wu <xiangwudev@gmail.com>
2018-04-28 14:12:50 -0400

    "added bubble chart"

--- client/src/CityComparison/CityComparisonContainer.jsx
+++ client/src/CityComparison/CityComparisonContainer.jsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import CityDropZone from './CityDropZone.jsx';
 import CityComparisonDataViz from './CityComparisonDataViz.jsx';
+import UsDataOverview from '../DataViz/UsDataOverview.jsx';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content : { 
+    width                 : '1400px',
+    height                : '900px',
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)',
+    background            : 'black',
+  }
+};
 
 class CityComparisonContainer extends React.Component {
   constructor(props) {
@@ -11,8 +27,14 @@ class CityComparisonContainer extends React.Component {
         right: ''
       },
       isDisplay: true,
+      modalIsOpen: false,
     }
     this.handleDrop = this.handleDrop.bind(this);
+
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.handleUserNameInput = this.handleUserNameInput.bind(this);
   }
 
   handleDrop(city, position) {
@@ -20,10 +42,27 @@ class CityComparisonContainer extends React.Component {
       selectedCities: Object.assign({}, this.state.selectedCities, {[position]: city})
     })
   }
-
-  hideChart() {
-
+  //modal methods
+  openModal(e) {
+    this.setState({ modalIsOpen: true });
   }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    this.subtitle.style.color = '#f00';
+  }
+
+  closeModal() {
+    this.setState({ modalIsOpen: false });
+  }
+
+  handleUserNameInput(ev) {
+    this.setState({
+      username: ev.target.value
+    });
+    console.log('username', this.state.username);
+  }
+  //end modal methods
 
   render() {
     const {left, right} = this.state.selectedCities;
@@ -31,7 +70,8 @@ class CityComparisonContainer extends React.Component {
     return (
     <div className="city-comparison-container">
       <div className="city-drop-zones">
-        <button className="bubble-map-viz">
+        <button className="bubble-map-viz" onClick={this.openModal}>
+        Bubble Chart
         </button>
         <button className="hide-chart" onClick={()=>{this.setState({isDisplay: !this.state.isDisplay})}}>
           hide
@@ -54,6 +94,18 @@ class CityComparisonContainer extends React.Component {
       <div className="city-data-viz">
         <CityComparisonDataViz left={left} right={right} isDisplay={this.state.isDisplay}/>
       </div>
+      <Modal
+            isOpen={this.state.modalIsOpen}
+            onAfterOpen={this.afterOpenModal}
+            onRequestClose={this.closeModal}
+            style={customStyles}
+            contentLabel="Example Modal"
+          >
+
+            <h2 ref={subtitle => this.subtitle = subtitle}>Bubble Chart</h2>
+            {this.props.cities.length && <UsDataOverview cities={this.props.cities}/>}
+            <button onClick={this.closeModal}>close</button>
+          </Modal>
     </div>
     )
   }
